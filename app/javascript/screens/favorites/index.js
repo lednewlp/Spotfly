b0db0d25326849cf8ed245a081530c5f9479cba1
Wendel Lopes dos Santos Filho <wendel@pop-os.localdomain>
2020-04-28 19:27:41 -0300

    Rotas

---
+++ app/javascript/screens/favorites/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+
+const FavoriteScreen = () => {
+  return(
+    <Fragment>
+      <div>Favorite Screen</div>
+    </Fragment>
+  )
+}
+export default FavoriteScreen;
