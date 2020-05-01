6aed92a1bf3123b8a28ef71fdc9761af3f276c57
Wendel Lopes dos Santos Filho <wendel@pop-os.localdomain>
2020-04-30 23:28:12 -0300

    Barra de Navegacao

--- app/javascript/screens/favorites/index.js
+++ app/javascript/screens/favorites/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
+
 
 const FavoriteScreen = () => {
   return(
     <Fragment>
       <div>Favorite Screen</div>
+      <NavbarFooter/>
     </Fragment>
   )
 }
--- app/javascript/screens/search/index.js
+++ app/javascript/screens/search/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
+import NavbarFooter from '../../components/common/navbar_footer';
+
 
 const SearchScreen = () => {
   return(
     <Fragment>
       <div>Search Screen</div>
+      <NavbarFooter/>
     </Fragment>
   )
 }
